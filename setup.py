a69147762662bcf0e2fa052397997acdd5f1e465
Mitchell Gottlieb <mgottlieb27@gmail.com>
2024-02-19 13:26:19 -0500

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyenvsync",
-    version="0.1.0",
+    version="0.0.1",
     description=("PySyncEnv assists with synchronizing and validating Python workspaces "
                  "across users and devices, ensuring seamless collaboration and consistent setups."),
     long_description=open("README.md").read(),
